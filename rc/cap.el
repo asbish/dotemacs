187007fefbe7ba45e099bd49c6c06eaf00da9278
Yu Ishii <asobuishii@gmail.com>
2019-12-30 02:49:29 +0900

    Fix ruby keybinding

--- rc/cap.el
+++ rc/cap.el
@@ -974,6 +974,7 @@
   (add-to-list 'auto-mode-alist '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
   (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
   :config
+  (define-key enh-ruby-mode-map (kbd "C-c /") nil)
   (define-key enh-ruby-mode-map (kbd "M-RET") 'lsp-rename))
 
 (use-package bundler :ensure t)
